279d3518b62c5eb3031b53b151696bda560fc17f
Anton Kruglyakov <akrugs@mail.ru>
2023-03-08 01:37:28 +0500

    Программа для решения задачи 2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача2
+//*/
+Console.Write("Введите первое число: ");
+int A= int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int B= int.Parse(Console.ReadLine()!);
+if (A>B) {
+    Console.Write($"Большее число равно {A}, а ");
+    Console.Write($"меньшее число равно {B}");
+} else if (A<B) {
+    Console.Write($"Большее число равно {B}, а ");
+    Console.Write($"меньшее число равно {A}");
+} else {
+    Console.Write("Введенные числа равны!");
+}
+//*/
